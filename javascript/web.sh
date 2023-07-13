02fa6361126b3f0135659ba390f64f37d88dccdd
renaatd <>
2023-07-13 21:07:06 +0200

    JavaScript: make scripts more consistent

--- javascript/web.sh
+++ javascript/web.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 . "$(dirname -- "$0")/settings.sh"
-echo "OUTPUT_FOLDER: ${OUTPUT_FOLDER}"
+
 mkdir -p "${OUTPUT_FOLDER}"
-cp index.html emscripten_helpers.mjs "${OUTPUT_FOLDER}"
+cp emscripten_helpers.mjs index.html "${OUTPUT_FOLDER}"
 cd "${OUTPUT_FOLDER}" || exit 1
 echo "Open the browser and point to http://localhost:8000/"
 python3 -m http.server
